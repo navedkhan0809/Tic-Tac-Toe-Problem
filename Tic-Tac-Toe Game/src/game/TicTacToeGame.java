936cdca4260e149e5acab3b676213aaa6bac6434
Naved Khan <navedkhan0809@gmail.com>
2021-04-20 12:54:04 +0530

    UC2-TicTacToe game choosing sign is added

--- Tic-Tac-Toe Game/src/game/TicTacToeGame.java
+++ Tic-Tac-Toe Game/src/game/TicTacToeGame.java
@@ -1,7 +1,59 @@
 package game;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic-Tac-Toe game problem");
+		
+		String comp = null;
+		char[][] board = new char[3][3];
+		
+		System.out.println("Welcome to Tic-Tac-Toe Game");
+		System.out.println("---------------------------");
+		choosesign();
+			
+		System.out.println("fresh tic-tac-toe board is");
+		drawboard(board);
+			
 	}
+
+	/**
+	 * 
+	 */
+	private static void choosesign() {
+		String comp;
+		System.out.println("You are player1,please choose x OR o");
+		Scanner in = new Scanner(System.in);
+		String p1 = in.nextLine();
+			
+			switch(p1) {
+				case "o":
+					p1 = "o";
+					comp = "x";
+					System.out.println("You(player1) choosed 'o' and comp choosed 'x'");
+					break;
+				case "x":
+					p1 = "x";
+					comp = "o";
+					System.out.println("You choosed 'x' and comp choosed 'o'");
+					break;
+				default:
+					System.out.println("Enter valid value");
+				}
+		
+	}
+
+		/**
+		 * @param board
+		 */
+		private static void drawboard(char[][] board) {
+			for (int i=0; i<3; i++) {
+				
+				for (int j=0; j<3; j++) {
+					board[i][j] = '-';
+					System.out.print(board[i][j]);
+				}
+				System.out.println();
+			}
+		}
 }
