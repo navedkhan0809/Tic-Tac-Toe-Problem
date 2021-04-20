1416accc609f828d515349c1f0764d3b8cdd8230
Naved Khan <navedkhan0809@gmail.com>
2021-04-20 12:10:31 +0530

    UC1 Added for board creation

--- Tic-Tac-Toe Game/src/game/TicTacToeGame.java
+++ Tic-Tac-Toe Game/src/game/TicTacToeGame.java
@@ -1,7 +1,25 @@
 package game;
 
-public class TicTacToeGame {
+class TicTacToeGame {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic-Tac-Toe game problem");
+	System.out.println("fresh tic-tac-toe board");
+	char[][] board = new char[3][3];
+	
+		drawboard(board);
+	}
+
+	/**
+	 * @param board
+	 */
+	private static void drawboard(char[][] board) {
+		for (int i=0; i<3; i++) {
+			
+			for (int j=0; j<3; j++) {
+				board[i][j] = '-';
+				System.out.print(board[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
+
